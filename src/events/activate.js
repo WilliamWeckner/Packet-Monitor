7cc7ec47a98dd9a49e040d99ce93202d0ae69ec2
Shaade <s.shaade77@gmail.com>
2019-10-06 16:25:52 +0200

    Update

--- src/events/activate.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app, win) => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-}
--- src/events/window-all-closed.js
+++
@@ -1,9 +0,0 @@
-// Quit when all windows are closed.
-module.exports = (app, win) => {
-
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-}
